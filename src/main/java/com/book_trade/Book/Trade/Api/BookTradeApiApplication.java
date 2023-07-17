452950da34e0e5bcc3f9364bf40926bc98e149e1
justjaee <113714562+justjaee@users.noreply.github.com>
2023-07-17 19:40:33 -0400

    Add files via upload

---
+++ src/main/java/com/book_trade/Book/Trade/Api/BookTradeApiApplication.java
@@ -0,0 +1,13 @@
+package com.book_trade.Book.Trade.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTradeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book_trade/Book/Trade/Api/BookTradeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_trade.Book.Trade.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTradeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
